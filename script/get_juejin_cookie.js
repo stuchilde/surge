cac1afb840890a94bb6c77723b219edf73f87744
yin.zhang <yin.zhang@mihoyo.com>
2021-12-11 13:24:51 +0800

    增加获取掘金cookie脚本

---
+++ script/get_juejin_cookie.js
@@ -0,0 +1,51 @@
+const cookieName = 'jue_jin'
+
+const cookieKey = 'jue_jin_cookie_key'
+
+const fun = init()
+
+const cookieVal = $request.headers['Cookie']
+
+if (cookieVal) {
+    if (fun.setData(cookieKey, cookieVal)) {
+        fun.msg(`${cookieName}`, '获取cookie成功', '')
+        fun.log(`[${cookieName}]获取cookie成功，cookie: ${cookieVal}`)
+    }
+}
+
+function init() {
+    isSurge = () => {
+        return undefined !== this.$httpClient
+    };
+    isQuanX = () => {
+        return undefined !== this.$task
+    }
+    getData = (key) => {
+        if (isSurge()) return $persistentStore.read(key)
+        if (isQuanX()) return $prefs.valueForKey(key)
+    }
+    setData = (key, value) => {
+        if (isSurge()) return $persistentStore.write(key, val)
+        if (isQuanX()) return $prefs.setValueForKey(key, val)
+    }
+    msg = (title, subTitle, body) => {
+        if (isSurge()) $notification.post(title, subTitle, body)
+        if (isQuanX()) $notify(title, subTitle, body)
+    }
+    log = (message) => {
+        console.log(message)
+    }
+    done = (value = {}) => {
+        $done(value)
+    }
+    return {
+        isSurge,
+        isQuanX,
+        getData,
+        setData,
+        log,
+        msg,
+        done,
+    }
+}
+fun.done()
